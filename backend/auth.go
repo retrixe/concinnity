a91860f91b0e695751a37643859f58593aa16b50
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2022-02-14 01:46:14 +0530

    backend: validate account details in register API

--- backend/auth.go
+++ backend/auth.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"regexp"
 	"time"
 
 	"github.com/google/uuid"
@@ -193,7 +194,18 @@ func RegisterEndpoint(w http.ResponseWriter, r *http.Request) {
 		} else if data.Username == "" || data.Password == "" || data.Email == "" {
 			http.Error(w, errorJson("No username, e-mail or password provided!"), http.StatusBadRequest)
 			return
-		} // TODO: Check if username, password and e-mail are valid.
+		} else if res, _ := regexp.MatchString("^[a-zA-Z0-9_]{4,16}$", data.Username); !res {
+			http.Error(w, errorJson("Username should be 4-16 characters long, and "+
+				"contain alphanumeric characters or _ only!"), http.StatusBadRequest)
+			return
+		} else if res, _ := regexp.MatchString("^.{8,64}$", data.Password); !res {
+			http.Error(w, errorJson("Your password must be between 8 and 64 characters long!"),
+				http.StatusBadRequest)
+			return
+		} else if res, _ := regexp.MatchString("^\\S+@\\S+\\.\\S+$", data.Email); !res {
+			http.Error(w, errorJson("Invalid e-mail entered!"), http.StatusBadRequest)
+			return
+		}
 		// Check if an account with this username or email already exists.
 		var u User
 		err = findUserByEmailStmt.QueryRow(data.Email).Scan(&u.Username, &u.Password, &u.Email, &u.ID, &u.Verified)
