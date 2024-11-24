a272e43f0c627fc97e2a834891a720e14e367713
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-11-25 02:53:07 +0530

    backend: merge password utils

--- backend/utils.go
+++ backend/utils.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
+
+	"golang.org/x/crypto/argon2"
 )
 
 func errorJson(err string) string {
@@ -16,4 +21,27 @@ func errorJson(err string) string {
 func handleInternalServerError(w http.ResponseWriter, err error) {
 	log.Println(err)
 	http.Error(w, errorJson("Internal Server Error!"), http.StatusInternalServerError)
+}
+
+// GenerateSalt returns a 16-character salt readable in UTF-8 format as well.
+func GenerateSalt() []byte {
+	saltBytes := make([]byte, 12)
+	_, _ = rand.Read(saltBytes)
+	salt := base64.RawStdEncoding.EncodeToString(saltBytes)
+	return []byte(salt)
+}
+
+func HashPassword(password string, salt []byte) string {
+	params := "$argon2id$v=19$m=51200,t=1,p=4$" // Currently fixed only.
+	key := argon2.IDKey([]byte(password), salt, 1, 51200, 4, 32)
+	return params + base64.RawStdEncoding.EncodeToString(salt) +
+		"$" + base64.RawStdEncoding.EncodeToString(key)
+}
+
+func ComparePassword(password string, hash string) bool {
+	encodeSplit := strings.Split(hash, "$")
+	salt, _ := base64.RawStdEncoding.DecodeString(encodeSplit[len(encodeSplit)-2])
+	key := argon2.IDKey([]byte(password), salt, 1, 51200, 4, 32)
+	hashValue := encodeSplit[len(encodeSplit)-1]
+	return hashValue == base64.RawStdEncoding.EncodeToString(key)
 }
