b5437e4e7b81d3c88e4b7f2f76b34d99c100debf
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-12-28 05:08:00 +0530

    frontend: add meta tags, Sass, top bar, favicon

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,89 @@
+<script lang="ts">
+  import { page } from '$app/state'
+  import type { Snippet } from 'svelte'
+
+  const { title, description, noIndex } = page.data
+  const { children }: { children: Snippet } = $props()
+</script>
+
+<svelte:head>
+  <title>{title ?? 'concinnity'}</title>
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content={title} />
+  <meta property="og:url" content={page.url.origin + page.url.pathname} />
+  <meta property="og:description" content={description} />
+  <meta name="Description" content={description} />
+  {#if noIndex}
+    <meta name="robots" content="noindex,nofollow" />
+  {/if}
+</svelte:head>
+
+<div class="top-bar">
+  <h1>concinnity</h1>
+  <!-- FIXME: Improve the design of this -->
+  <a href="/login">Login</a>
+</div>
+
+<div style:margin-top="4rem"></div>
+
+{@render children()}
+
+<style lang="scss">
+  :global {
+    * {
+      margin: 0;
+    }
+
+    @media (prefers-color-scheme: dark) {
+      :root {
+        color-scheme: dark;
+        --link-color: #8f00ff;
+        --background-color: #111;
+        --color: #ffffff;
+        --divider-color: #666;
+      }
+    }
+
+    @media (prefers-color-scheme: light) {
+      :root {
+        --link-color: #8f00ff;
+        --background-color: #eee;
+        --color: #000000;
+        --divider-color: #bbb;
+      }
+    }
+
+    body {
+      font-family: system-ui, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue',
+        Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+      background-color: var(--background-color);
+      color: var(--color);
+      width: 100vw;
+      max-width: 100%;
+      min-height: 100vh;
+      display: flex;
+      flex-direction: column;
+      a {
+        color: var(--link-color);
+      }
+    }
+  }
+
+  .top-bar {
+    position: fixed;
+    display: flex;
+    width: 100%;
+    box-sizing: border-box;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem;
+    backdrop-filter: blur(20px);
+    border-bottom: 1px solid var(--divider-color);
+    h1 {
+      font-size: 1.5rem;
+    }
+    a {
+      text-decoration: none;
+    }
+  }
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="container">
+  <h1>Welcome to SvelteKit</h1>
+  <p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+  </p>
+</div>
+
+<style lang="scss">
+  .container {
+    display: flex;
+    padding: 4rem;
+    justify-content: space-evenly;
+    align-items: center;
+    flex-grow: 1;
+  }
+</style>
