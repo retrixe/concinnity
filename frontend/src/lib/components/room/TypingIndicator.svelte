58aab5a81680a696b031663bed2ffc730f8310d7
Subhayan Das <subhayan.sd10@gmail.com>
2025-01-24 18:51:12 +0530

    feat: add typingindicator component

---
+++ frontend/src/lib/components/room/TypingIndicator.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  interface Props {
+    typingUsers: string[]
+  }
+
+  const { typingUsers }: Props = $props()
+</script>
+
+<div class="typing-indicator">
+  {#if typingUsers.length >= 3}
+    <p>Multiple users are typing...</p>
+  {:else if typingUsers.length === 2}
+    <p>{`${typingUsers[0]} and ${typingUsers[1]} are typing...`}</p>
+  {:else}
+    {#each typingUsers as userKey}
+      <p>{`${userKey} is typing...`}</p>
+    {/each}
+  {/if}
+</div>
+
+<style lang="scss">
+  .typing-indicator {
+    font-size: 1rem;
+  }
+</style>
