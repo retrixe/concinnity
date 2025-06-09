6c609b6f6ba04677eabe26c4d0a79ce63e640e32
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-06-09 21:18:20 +0530

    feat(frontend): iterate on account settings

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { goto } from '$app/navigation'
   import { page } from '$app/state'
-  import { Box } from 'heliodor'
+  import { Box, Button, TextInput } from 'heliodor'
 
   const { userId, username, email } = $derived(page.data)
 
@@ -9,27 +9,65 @@
     if (!username) goto('/login', { replaceState: true }).catch(console.error)
   })
 
-  // TODO: Add change username functionality
-  // TODO: Add change email functionality
+  // TODO: Add change username functionality with dialog
+  // TODO: Add change email functionality with dialog
   // TODO: Add change password functionality
-  // TODO: Add delete account functionality
+  // TODO: Add delete account functionality with dialog
 </script>
 
 <div class="container">
   <h1>Account Settings</h1>
+
   <Box class="content">
-    <h4>Username</h4>
-    <h2>{username}</h2>
+    <div class="space-between">
+      <div>
+        <h4>Username</h4>
+        <h2>{username}</h2>
+      </div>
+      <Button>Edit</Button>
+    </div>
     <br />
-    <h4>Email</h4>
-    <p>{email}</p>
+    <div class="space-between">
+      <div>
+        <h4>Email</h4>
+        <p>{email}</p>
+      </div>
+      <Button>Edit</Button>
+    </div>
     <br />
     <h4>Account ID</h4>
     <p>{userId}</p>
   </Box>
+
+  <Box class="content">
+    <h3>Change Password</h3>
+    <br />
+    <TextInput class="changepw" type="password" placeholder="Enter your current password" />
+    <TextInput class="changepw" type="password" placeholder="Enter new password" />
+    <TextInput class="changepw" type="password" placeholder="Confirm password" />
+    <div class="changepw-button">
+      <Button>Change Password</Button>
+    </div>
+  </Box>
+
+  <Box class="content">
+    <Button class="error">Delete Account</Button>
+  </Box>
 </div>
 
 <style lang="scss">
+  :global(.changepw) {
+    display: block;
+  }
+
+  .changepw-button {
+    margin-top: 24px;
+  }
+
+  :global(.error) {
+    background-color: var(--error-color) !important;
+  }
+
   @mixin content {
     margin-top: 32px;
     width: 100%;
@@ -43,6 +81,12 @@
 
   .container > h1 {
     @include content();
+  }
+
+  .space-between {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 
   .container {
