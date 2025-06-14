fba164952ad11b81776f79594c92b41daa0b0a89
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-06-14 23:32:02 +0530

    feat(frontend): finish delete account in settings

--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
+  import { Box, Button } from 'heliodor'
   import { goto } from '$app/navigation'
   import { page } from '$app/state'
-  import { Box, Button, TextInput } from 'heliodor'
+  import DeleteAccountDialog from './DeleteAccountDialog.svelte'
 
   const { userId, username, email } = $derived(page.data)
 
@@ -9,10 +10,12 @@
     if (!username) goto('/login', { replaceState: true }).catch(console.error)
   })
 
+  let currentDialog: 'changeUsername' | 'changeEmail' | 'changePassword' | 'deleteAccount' | null =
+    $state(null)
+
+  // TODO: Add change password functionality with dialog
   // TODO: Add change username functionality with dialog
   // TODO: Add change email functionality with dialog
-  // TODO: Add change password functionality
-  // TODO: Add delete account functionality with dialog
 </script>
 
 <div class="container">
@@ -24,7 +27,7 @@
         <h4>Username</h4>
         <h2>{username}</h2>
       </div>
-      <Button>Edit</Button>
+      <!-- <Button>Edit</Button> -->
     </div>
     <br />
     <div class="space-between">
@@ -32,7 +35,7 @@
         <h4>Email</h4>
         <p>{email}</p>
       </div>
-      <Button>Edit</Button>
+      <!-- <Button>Edit</Button> -->
     </div>
     <br />
     <h4>Account ID</h4>
@@ -40,47 +43,36 @@
   </Box>
 
   <Box class="content">
-    <h3>Change Password</h3>
-    <br />
-    <TextInput class="changepw" type="password" placeholder="Enter your current password" />
-    <TextInput class="changepw" type="password" placeholder="Enter new password" />
-    <TextInput class="changepw" type="password" placeholder="Confirm password" />
-    <div class="changepw-button">
-      <Button>Change Password</Button>
-    </div>
-  </Box>
-
-  <Box class="content">
-    <Button class="error">Delete Account</Button>
+    <Button class="gap-right" onclick={() => (currentDialog = 'changePassword')}>
+      Change Password
+    </Button>
+    <Button class="error" onclick={() => (currentDialog = 'deleteAccount')}>Delete Account</Button>
   </Box>
 </div>
 
+<DeleteAccountDialog
+  open={currentDialog === 'deleteAccount'}
+  onClose={() => (currentDialog = null)}
+/>
+
 <style lang="scss">
-  :global(.changepw) {
-    display: block;
+  :global(.gap-right) {
+    margin-right: 12px;
   }
 
-  .changepw-button {
-    margin-top: 24px;
-  }
-
-  :global(.error) {
+  :global(button.error) {
     background-color: var(--error-color) !important;
-  }
-
-  @mixin content {
-    margin-top: 32px;
-    width: 100%;
-    max-width: 600px;
   }
 
   .container > :global(.content) {
     padding: 16px;
-    @include content();
   }
 
-  .container > h1 {
-    @include content();
+  .container > h1,
+  :global(.content) {
+    margin-top: 32px;
+    width: 100%;
+    max-width: 600px;
   }
 
   .space-between {
---
+++ frontend/src/routes/settings/DeleteAccountDialog.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+  import { goto, invalidate } from '$app/navigation'
+  import ky from '$lib/api/ky'
+  import { Button, Dialog, TextInput } from 'heliodor'
+
+  const { open, onClose } = $props<{
+    open: boolean
+    onClose: () => void
+  }>()
+
+  let currentPassword = $state('')
+  let disabled = $state(false)
+  let error: string | null = $state(null)
+
+  const clearError = () => (error = null)
+
+  async function handleDeleteAccount() {
+    disabled = true
+    try {
+      await ky.delete(`api/delete-account`, { json: { currentPassword } }).json()
+      error = ''
+      // Don't bother cleaning up the timeout, what if the user closes the dialog?
+      localStorage.removeItem('concinnity:token')
+      setTimeout(() => goto('/').then(() => invalidate('app:auth')), 3000)
+    } catch (e: unknown) {
+      error = e instanceof Error ? e.message : (e?.toString() ?? `Failed to delete account!`)
+    }
+    disabled = false
+  }
+</script>
+
+<Dialog {open} {onClose}>
+  <h2>Delete Account</h2>
+  <br />
+  <p>
+    Enter your password below and press "Confirm" to delete your account.
+    <span style:color="var(--error-color)">
+      This will delete your Concinnity data permanently and cannot be undone.
+    </span>
+  </p>
+  <label for="delete-account-password">Current Password</label>
+  <TextInput
+    id="delete-account-password"
+    bind:value={currentPassword}
+    oninput={clearError}
+    error={!!error}
+    {disabled}
+    type="password"
+    placeholder="Enter your password"
+  />
+  {#if error}
+    <p class="error">{error}</p>
+  {:else if error === ''}
+    <p>
+      Account deleted successfully! You will be redirected to the <a href="/">homepage</a> shortly...
+    </p>
+  {/if}
+  <div style:margin-top="1rem"></div>
+  <Button class="delete-account-btn" onclick={handleDeleteAccount}>Confirm</Button>
+</Dialog>
+
+<style lang="scss">
+  .error {
+    color: var(--error-color);
+  }
+
+  :global(.delete-account-btn) {
+    align-self: flex-end;
+  }
+
+  label {
+    padding: 0.5rem 0rem;
+    font-weight: bold;
+  }
+</style>
