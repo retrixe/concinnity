82212460e7c94eeaf79e474e36a5d9a8c951756b
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2025-12-29 01:20:40 +0530

    fix(frontend): add Vite devtools-json plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 import { sveltePhosphorOptimize } from 'phosphor-svelte/vite'
 
 export default defineConfig({
-  plugins: [sveltekit(), sveltePhosphorOptimize()],
+  plugins: [sveltekit(), sveltePhosphorOptimize(), devtoolsJson()],
 })
